458fef0294b7f414cecfc61f1e88638fc85100fb
Ro0t-set <ro0trexustom@gmail.com>
2018-08-26 21:04:49 +0200

    bella bug corretto

--- app/views.py
+++ app/views.py
@@ -321,8 +321,8 @@ def filtro_fasce(request):
     cerca = request.GET.get("q")
 
     if corsi in ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9']:
-        meta = corsi
-        eval('corsi = Corso.objects.filter('+meta+'=True).order_by(\'titolo\')')
+        meta = str(corsi)
+        corsi= eval('Corso.objects.filter('+meta+'=True).order_by("titolo")')
         fascia = corsi
     return render(request, 'corsi/filtro_fasce.html', {'corsi' : corsi, 'fascia': fascia, 'form':form})
 
